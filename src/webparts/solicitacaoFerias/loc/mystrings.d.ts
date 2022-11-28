f8c02dd84fc11208558f1f4d0371c48db3bf0965
Victor Castro <victor.castro@saber.parceirosedu.com.br>
2022-11-28 15:44:22 -0300

    initial commit

---
+++ src/webparts/solicitacaoFerias/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISolicitacaoFeriasWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SolicitacaoFeriasWebPartStrings' {
+  const strings: ISolicitacaoFeriasWebPartStrings;
+  export = strings;
+}
